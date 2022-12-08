a74c2833c3dfd0cb7d5fabc189a5e0effc9ecb91
AbhishekPandey <askabhishek16@gmail.com>
2022-12-08 13:09:42 +0530

    added base and util class

--- nrcautomation/src/main/java/com/inn/nrc/base/TestBase.java
+++ nrcautomation/src/main/java/com/inn/nrc/base/TestBase.java
@@ -1,5 +1,71 @@
 package com.inn.nrc.base;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import com.aventstack.extentreports.ExtentTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestBase {
+	
+	public static WebDriver driver;
+	public static Properties prop;
+	public static ExtentTest test;
+	
+
+	public TestBase()   {
+		
+		try {
+			prop= new Properties();
+			FileInputStream fis = new FileInputStream("C:\\software\\team_project_att\\nrcautomation\\src\\main\\java\\com\\inn\\nrc\\config\\confi.properties");
+			prop.load(fis);
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		catch (IOException e) {
+			e.printStackTrace();
+		}	
+
+	}
+
+	public static void initilization()
+	{
+		
+			String browser=prop.getProperty("browser");
+			
+			if (browser.equalsIgnoreCase("chrome"))
+			{
+				WebDriverManager.chromedriver().setup();
+				driver= new ChromeDriver();
+			}
+			else
+				if(browser.equalsIgnoreCase("edge"))
+				{
+					WebDriverManager.edgedriver().setup();
+					driver=new EdgeDriver();
+				}
+
+		//		driver.manage().deleteAllCookies();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.MINUTES);
+			driver.manage().timeouts().implicitlyWait(2, TimeUnit.MINUTES);//its a global wait applicable to all WebElement which driver interact
+			driver.get(prop.getProperty("url"));
+		
+	
+	}
+
 
 }
---
+++ nrcautomation/src/main/java/com/inn/nrc/util/TestUtil.java
@@ -0,0 +1,27 @@
+package com.inn.nrc.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class TestUtil {
+
+
+	public static String takeScreenshotAtEndOfTestFromBase64() throws IOException
+	{
+		File scrFile=((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String currentDir = System.getProperty("user.dir");
+		File descFile=new File(currentDir+"//screenshots"+System.currentTimeMillis()+".png");
+		FileUtils.copyFile(scrFile, descFile);
+		byte[] imageByte =IOUtils.toByteArray(new FileInputStream(descFile)); 
+		return Base64.getEncoder().encodeToString(imageByte);
+	}
+
+
+}
