5fa5682ca281944df6bd30e111fe1449cb7551e2
Reddy <kondeti.reddy@rakuten.com>
2022-12-09 16:02:01 +0530

    small changes for abhishem

--- nrcautomation/src/main/java/com/inn/nrc/pages/CaptureMacroRF.java
+++ nrcautomation/src/main/java/com/inn/nrc/pages/CaptureMacroRF.java
@@ -3,10 +3,21 @@ package com.inn.nrc.pages;
 import org.openqa.selenium.By;
 
 import com.inn.nrc.base.TestBase;
+import com.inn.nrc.util.TestUtil;
 
 public class CaptureMacroRF extends TestBase{
 	By basicdetails=By.xpath("//mat-panel-title[normalize-space()='Basic Details']");
 	By allRequiredSupportingDocumentationBeenUploadedRadioBtn=By.xpath("//label[@for='mat-radio-51-input']//span[@class='mat-radio-outer-circle']");
-	 
+	
+	
+	
+	
+	
+public void capturemacrorf() {
+	TestUtil.waitTillElementVisible(basicdetails);
+	driver.findElement(basicdetails).click();
+	TestUtil.waitTillElementVisible(allRequiredSupportingDocumentationBeenUploadedRadioBtn);
+	driver.findElement(allRequiredSupportingDocumentationBeenUploadedRadioBtn).click();
+}
 
 }
